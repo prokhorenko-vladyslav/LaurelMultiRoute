63a3bc32e6d611ad5491796e63c59d15d37c9a79
Prokhorenko Vladyslav <random.person.1907@gmail.com>
2020-04-28 19:50:38 +0300

    Added service provider.

---
+++ src/Providers/MultiRouteServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Laurel\MultiRoute\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class MultiRouteServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
