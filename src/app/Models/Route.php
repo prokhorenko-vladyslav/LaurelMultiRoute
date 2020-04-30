561969cbd090913eaf029a87ef83d18c076beb56
Prokhorenko Vladyslav <random.person.1907@gmail.com>
2020-04-30 22:54:16 +0300

    Added relations to Route Model.

--- src/app/Models/Route.php
+++ src/app/Models/Route.php
@@ -3,8 +3,26 @@
 namespace Laurel\MultiRoute\App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 class Route extends Model
 {
     protected $fillable = ['slug'];
+
+    public function parent() : BelongsTo
+    {
+        return $this->belongsTo(self::class, 'parent_id');
+    }
+
+    public function children() : HasMany
+    {
+        return $this->hasMany(self::class, 'parent_id');
+    }
+
+    public function routable() : MorphTo
+    {
+        return $this->morphTo();
+    }
 }
