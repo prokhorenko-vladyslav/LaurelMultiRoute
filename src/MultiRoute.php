ab7c96deebc09573680c58bc2fd3509d3406c290
Prokhorenko Vladyslav <random.person.1907@gmail.com>
2020-05-01 19:28:41 +0300

    Added routes method

--- src/MultiRoute.php
+++ src/MultiRoute.php
@@ -4,16 +4,21 @@
 namespace Laurel\MultiRoute;
 
 
+use App\Http\Controllers\TestController;
+use Illuminate\Support\Facades\Route;
+
 class MultiRoute
 {
     public static function handle()
     {
-
+        //app()->call("App\Http\Controllers\TestController@index");
     }
 
     public static function routes()
     {
-
+        Route::namespace('\Laurel\MultiRoute')->group(function() {
+           Route::any('{path?}', 'MultiRoute@handle')->where('path', '.*')->name('multi-route.index');
+        });
     }
 
     public static function path()
