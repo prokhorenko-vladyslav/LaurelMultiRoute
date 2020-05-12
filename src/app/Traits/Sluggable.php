50b9105c7bb575b8533545cc4d57d0e44d4156ec
Prokhorenko Vladyslav <random.person.1907@gmail.com>
2020-05-12 23:51:56 +0300

    Added some comments.

--- src/app/Traits/Sluggable.php
+++ src/app/Traits/Sluggable.php
@@ -4,18 +4,41 @@
 namespace Laurel\MultiRoute\App\Traits;
 
 
+/**
+ * Trait for manipulating path slug
+ *
+ * Trait Sluggable
+ * @package Laurel\MultiRoute\App\Traits
+ */
 trait Sluggable
 {
+    /**
+     * Gets path by slug
+     *
+     * @param $slug
+     * @return static|null
+     */
     public static function getBySlug($slug) : ?self
     {
         return self::where('slug', $slug)->first();
     }
 
+    /**
+     * Gets path by slug or fail, if it has not been found
+     *
+     * @param $slug
+     * @return static|null
+     */
     public static function getBySlugOrFail($slug) : ?self
     {
         return self::where('slug', $slug)->firstOrFail();
     }
 
+    /**
+     * Sets path slug
+     *
+     * @param string $slug
+     */
     public function setSlugAttribute(string $slug)
     {
         $this->attributes["slug"] = $slug;
